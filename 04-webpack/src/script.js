a3b42e13143a886272477431a25d0de35353faf2
Diwakar Singh <101459162+itsdiwakarsingh@users.noreply.github.com>
2026-02-20 20:10:27 +0530

    completed 04-transform-objects

--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -1,1 +1,35 @@
-import './style.css'
+import "./style.css";
+import * as THREE from "three";
+console.log("Hello, Three.js Journey");
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Object
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({
+  color: "#ff0000",
+});
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cubeMesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
